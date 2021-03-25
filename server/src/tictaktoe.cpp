2a0444bd584b2247dc9641ffad2f7c8c07234b89
takeltez <mountain.skier@yandex.ru>
2021-03-25 19:42:51 +0700

    repetitive players connetcion, victory on last cell

--- server/src/tictaktoe.cpp
+++ server/src/tictaktoe.cpp
@@ -2,13 +2,9 @@
 
 char board[3][3];
 
-// char winner = -1;
-
 int movement;
 
 int listener;
-
-char current_player = 'X';
 
 vector<char> signs;
 
@@ -54,6 +50,8 @@ void *thread_handler(void *clients)
 {
     char sign;
 
+    char current_player = 'X';
+
     vector<int> new_clients = *(vector<int> *) clients;
 
     int curr_sock = new_clients[new_clients.size() - 1];
@@ -95,7 +93,7 @@ void *thread_handler(void *clients)
 
     movement = 0;
 
-    while(signs.size() != 2) //Waiting for opponent
+    while(signs.size() % 2) //Waiting for opponent
     {
         sleep(1);
     }
@@ -149,7 +147,7 @@ void *thread_handler(void *clients)
         if (winner != -1)
             break;
 
-        current_player = 'X' ? current_player == 'O' : 'O';
+        current_player == 'X' ? current_player = 'O' : 'X';
     }
 
     return 0;
@@ -172,9 +170,6 @@ bool border_validate(int number)
 
 char game_over_validate()
 {
-    if (movement >= 9)
-        return 0;
-    else
     if (movement >= 5)
     {
         if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][1] != ' ')
@@ -193,7 +188,13 @@ char game_over_validate()
             return board[1][1];
         else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
             return board[1][1];
+    }   
+    else
+    {
+        if (movement >= 9)
+            return 0;
     }
+
     return -1;
 }
 
