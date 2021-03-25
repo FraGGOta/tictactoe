2208187877f0b7a7f433115e97d8b77743f6b4db
takeltez <mountain.skier@yandex.ru>
2021-03-25 21:10:05 +0700

    fixed draw condition

--- server/src/tictaktoe.cpp
+++ server/src/tictaktoe.cpp
@@ -169,7 +169,7 @@ bool border_validate(int number)
 }
 
 char game_over_validate()
-{
+{   
     if (movement >= 5)
     {
         if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][1] != ' ')
@@ -189,11 +189,9 @@ char game_over_validate()
         else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
             return board[1][1];
     }   
-    else
-    {
-        if (movement >= 9)
-            return 0;
-    }
+
+    if (movement >= 9)
+        return 0;
 
     return -1;
 }
